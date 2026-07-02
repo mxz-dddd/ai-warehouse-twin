f8d674cfcff893792a1c61e202a50f16beb4eb02
马昕泽 <kevinknightmxz@gmail.com>
2026-07-02 18:00:12 +0800

    [Demo] EPIC-003-B: add actor controller and worker actor

---
+++ engine/unity/AIWarehouseTwin/Assets/Scripts/Agent/WorkerActor.cs
@@ -0,0 +1,118 @@
+using AIWarehouseTwin.World;
+using UnityEngine;
+
+namespace AIWarehouseTwin.Agent
+{
+    public sealed class WorkerActor : ActorController
+    {
+        private static readonly Color DefaultWorkerColor = new Color(0.29f, 0.56f, 0.85f, 1f);
+
+        [SerializeField]
+        private WarehousePalette palette;
+
+        [SerializeField]
+        private float bobAmplitude = 0.08f;
+
+        [SerializeField]
+        private float bobFrequency = 8f;
+
+        private Transform visualRoot;
+        private GameObject body;
+        private GameObject head;
+        private ActorState state = ActorState.Idle;
+
+        public float BobOffset { get; private set; }
+
+        public GameObject Body => body;
+
+        public GameObject Head => head;
+
+        public void SetPalette(WarehousePalette nextPalette)
+        {
+            palette = nextPalette;
+            ApplyColor();
+        }
+
+        public void EnsureVisuals()
+        {
+            if (visualRoot == null)
+            {
+                var root = new GameObject("WorkerVisuals");
+                root.transform.SetParent(transform, false);
+                visualRoot = root.transform;
+            }
+
+            if (body == null)
+            {
+                body = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                body.name = "WorkerBody";
+                body.transform.SetParent(visualRoot, false);
+                body.transform.localPosition = new Vector3(0f, 0.75f, 0f);
+                body.transform.localScale = new Vector3(0.35f, 0.75f, 0.35f);
+            }
+
+            if (head == null)
+            {
+                head = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                head.name = "WorkerHead";
+                head.transform.SetParent(visualRoot, false);
+                head.transform.localPosition = new Vector3(0f, 1.65f, 0f);
+                head.transform.localScale = Vector3.one * 0.35f;
+            }
+
+            ApplyColor();
+        }
+
+        protected override void OnStateChanged(ActorState next)
+        {
+            state = next;
+            if (state != ActorState.Moving)
+            {
+                SetBobOffset(0f);
+            }
+        }
+
+        protected override void OnTicked(float simTime)
+        {
+            if (state == ActorState.Moving)
+            {
+                SetBobOffset(Mathf.Sin(simTime * bobFrequency) * bobAmplitude);
+            }
+        }
+
+        private void Awake()
+        {
+            EnsureVisuals();
+        }
+
+        private void SetBobOffset(float offset)
+        {
+            BobOffset = offset;
+            if (visualRoot != null)
+            {
+                visualRoot.localPosition = new Vector3(0f, BobOffset, 0f);
+            }
+        }
+
+        private void ApplyColor()
+        {
+            var color = palette != null ? palette.worker : DefaultWorkerColor;
+            ApplyColor(body, color);
+            ApplyColor(head, color);
+        }
+
+        private static void ApplyColor(GameObject target, Color color)
+        {
+            if (target == null)
+            {
+                return;
+            }
+
+            var renderer = target.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.sharedMaterial.color = color;
+            }
+        }
+    }
+}
---
+++ engine/unity/AIWarehouseTwin/Assets/Tests/EditMode/WorkerActorTests.cs
@@ -0,0 +1,102 @@
+using AIWarehouseTwin.Agent;
+using AIWarehouseTwin.World;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace AIWarehouseTwin.Tests
+{
+    public sealed class WorkerActorTests
+    {
+        private GameObject gameObject;
+        private WarehousePalette palette;
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (gameObject != null)
+            {
+                Object.DestroyImmediate(gameObject);
+            }
+
+            if (palette != null)
+            {
+                Object.DestroyImmediate(palette);
+            }
+        }
+
+        [Test]
+        public void EnsureVisuals_creates_capsule_body_and_sphere_head()
+        {
+            var worker = CreateWorker();
+
+            worker.EnsureVisuals();
+
+            Assert.That(worker.Body, Is.Not.Null);
+            Assert.That(worker.Head, Is.Not.Null);
+            Assert.That(worker.Body.name, Is.EqualTo("WorkerBody"));
+            Assert.That(worker.Head.name, Is.EqualTo("WorkerHead"));
+            Assert.That(worker.Body.GetComponent<CapsuleCollider>(), Is.Not.Null);
+            Assert.That(worker.Head.GetComponent<SphereCollider>(), Is.Not.Null);
+        }
+
+        [Test]
+        public void EnsureVisuals_uses_palette_worker_color_when_provided()
+        {
+            var worker = CreateWorker();
+            palette = ScriptableObject.CreateInstance<WarehousePalette>();
+            palette.ApplyDefaultColors();
+
+            worker.SetPalette(palette);
+            worker.EnsureVisuals();
+
+            var bodyColor = worker.Body.GetComponent<Renderer>().sharedMaterial.color;
+            Assert.That(bodyColor.r, Is.EqualTo(palette.worker.r).Within(0.001f));
+            Assert.That(bodyColor.g, Is.EqualTo(palette.worker.g).Within(0.001f));
+            Assert.That(bodyColor.b, Is.EqualTo(palette.worker.b).Within(0.001f));
+        }
+
+        [Test]
+        public void Moving_state_updates_bob_offset_without_changing_route_position()
+        {
+            var worker = CreateWorker();
+            worker.LoadRoute(new[]
+            {
+                new ActorRouteSegment(0f, 2f, Vector3.zero, new Vector3(2f, 0f, 0f), ActorState.Moving),
+            });
+
+            worker.Tick(0.25f);
+            var firstBob = worker.BobOffset;
+            var positionAfterFirstTick = worker.transform.position;
+
+            worker.Tick(0.5f);
+
+            Assert.That(Mathf.Abs(worker.BobOffset - firstBob), Is.GreaterThan(0.0001f));
+            Assert.That(positionAfterFirstTick.y, Is.EqualTo(0f).Within(0.001f));
+            Assert.That(worker.transform.position.y, Is.EqualTo(0f).Within(0.001f));
+        }
+
+        [Test]
+        public void Non_moving_state_resets_bob_offset()
+        {
+            var worker = CreateWorker();
+            worker.LoadRoute(new[]
+            {
+                new ActorRouteSegment(0f, 1f, Vector3.zero, Vector3.right, ActorState.Moving),
+                new ActorRouteSegment(1f, 2f, Vector3.right, Vector3.right, ActorState.Picking),
+            });
+
+            worker.Tick(0.25f);
+            Assert.That(Mathf.Abs(worker.BobOffset), Is.GreaterThan(0f));
+
+            worker.Tick(1.5f);
+
+            Assert.That(worker.BobOffset, Is.EqualTo(0f).Within(0.001f));
+        }
+
+        private WorkerActor CreateWorker()
+        {
+            gameObject = new GameObject("WorkerActorTests");
+            return gameObject.AddComponent<WorkerActor>();
+        }
+    }
+}
